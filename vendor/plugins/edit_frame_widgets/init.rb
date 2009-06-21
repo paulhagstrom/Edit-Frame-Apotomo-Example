119f67aa2667216782f889648d879873b7055ea1
Paul Hagstrom <hagstrom@bu.edu>
2009-06-21 17:18:18 -0400

    removed edit_frame widgets, so I can install it as a submodule.

--- vendor/plugins/edit_frame_widgets/init.rb
+++
@@ -1,3 +0,0 @@
-# I have no idea what things here are needed.  I need to figure out what it takes to make a plugin
-module EditFrameWidgets
-end
--- vendor/plugins/edit_frame_widgets/install.rb
+++
@@ -1,2 +0,0 @@
-# Install hook code here
-# I have no idea what things here are needed.  I need to figure out what it takes to make a plugin
