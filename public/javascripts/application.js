79b34bc0c0acaf0da800e85529d949279e50f2f1
Paul Hagstrom <hagstrom@bu.edu>
2009-06-23 15:30:32 -0400

    About half of an implementation of a jQuery jqgrid-enabled table (publishers). I included most current versions of everything here as of now.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// from http://henrik.nyh.se/2008/05/rails-authenticity-token-with-jquery
+jQuery(document).ajaxSend(function(event, request, settings) {
+  if (typeof(AUTH_TOKEN) == "undefined") return;
+  // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
+  settings.data = settings.data || "";
+  settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+});
